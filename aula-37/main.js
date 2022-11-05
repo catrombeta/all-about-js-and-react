31d1426669736b1f52152b668d39e52329fa136e
Caroline Trombeta <carolinecampost@gmail.com>
2022-11-05 13:17:28 +0000

    05-11 New folder with content of class 37 - another way to use for (to read, not to increment. So, this way able to us read index or objects)

---
+++ aula-37/main.js
@@ -0,0 +1,9 @@
+const frutas = ['Pêra', 'Maçã', 'Uva'];
+
+// outro exemplo de for para LEITURA e não INCREMENTO. Ou seja, lê os índices ou objeto
+
+for (let i in frutas) {
+    console.log(i);
+    console.log(frutas[i]);
+} 
+
