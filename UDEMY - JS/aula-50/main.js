c32d90acc11c0b0972dbd6906c8bc39dda277720
Caroline Trombeta <carolinecampost@gmail.com>
2022-11-07 23:33:06 +0000

    07/11 - Class about return of functions

---
+++ UDEMY - JS/aula-50/main.js
@@ -0,0 +1,11 @@
+// RETORNO DE FUNÇÕES: RETURN - RETORNA UM VALOR, OU SEJA, A FUNÇÃO TERMINA ALÍ
+
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(5,2));
+
+function criaPessoa (nome, sobrenome) {
+    return {nome, sobrenome};
+}
+
