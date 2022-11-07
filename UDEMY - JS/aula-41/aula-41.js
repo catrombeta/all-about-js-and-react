1f2f9ccfb95fe307e35b7cb3fc146091bef02478
Caroline Trombeta <carolinecampost@gmail.com>
2022-11-07 13:14:30 +0000

    07/11 - I made a little exercise to return the bigger number

---
+++ UDEMY - JS/aula-41/aula-41.js
@@ -0,0 +1,22 @@
+// function biggestNumer (n1, n2) {
+
+//     const n1 = 5;
+//     const n2 = 10;
+
+//     if (n1 > n2) {
+//         console.log(n1);
+//     } if (n2 > n1) {
+//         console.log(n2);
+//     }
+// }
+
+// console.log(biggestNumer());
+
+function max (x, y) {
+    if (x > y) {
+        return x;
+    }
+    return y;
+}
+
+console.log(max(1,2));
